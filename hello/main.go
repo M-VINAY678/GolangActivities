ec7c7b7bea214a5b03af752de4c5ae50b26d0fe9
vinay <vinay.marumamula@beautifulcode.in>
2025-09-19 10:10:29 +0530

    changes in rough work branch

--- hello/main.go
+++ hello/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello,Go!")
+	//fmt.Println("Hello,Go!")
+	var w writer = ConsoleWriter{}
+	w.write([]byte("hello world"))
+}
+
+type writer interface {
+	write([]byte) (int, error)
+}
+
+type ConsoleWriter struct {
+}
+
+func (cw ConsoleWriter) write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
 }
